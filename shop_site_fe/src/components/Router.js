cfc9a846dad200bd35914365c972b92b0f53d5a8
Trey McGarity <trey.mcgarity99@gmail.com>
2021-01-19 19:15:18 -0600

    file structure setup

---
+++ shop_site_fe/src/components/Router.js
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import './styles/router.css';
+
+function Routing() {
+  return (
+    <Router className="router">
+
+    </Router>   
+  );
+}
+
+export default Routing;
