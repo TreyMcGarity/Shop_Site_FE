50ef32209f79f796d51f56b1b1a90821b5735bed
Trey McGarity <trey.mcgarity99@gmail.com>
2021-01-24 17:17:13 -0600

    setting up for home page

--- shop_site_fe/src/components/Header.jsx
+++ shop_site_fe/src/components/Header.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
-import { connect } from "react-redux";
+import React, { useState } from 'react';
+// import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+// import { getProduct } from '../redux/actions/productActions';
 import '../styles/header.css';
 
 const Header = () => {
+    // const dispatch = useDispatch();
+    const [search, setSearch] = useState("");
+	const handleChange = (e) => {
+		setSearch({ ...search, [e.target.name]: e.target.value });
+	};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		// dispatch(getProduct(search));
+	};
     return (
-        <div className='header.css'>
+        <div className='header'>
             <div>
                 <h1>DECOREM</h1>
-                <Link to='/profile'>Profile</Link>
+                <Link to='/listing'>Listing</Link>
             </div>
             <div>
-                <Link to='/listing'>Listing</Link>
-                <form>
-                    <inpt></inpt>
+                <Link to='/profile'>Profile</Link>
+                <form onSubmit={handleSubmit}>
+                    <input
+						type="text"
+                        name="email"
+                        placeholder="search"
+						value={search}
+						onChange={handleChange}
+					/>
                 </form>
             </div>
         </div>
