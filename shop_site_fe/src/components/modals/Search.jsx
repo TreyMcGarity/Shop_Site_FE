a78eae340a7417b9f0e7454b610fa055a1dd80f2
Trey McGarity <trey.mcgarity99@gmail.com>
2021-02-01 11:09:10 -0600

    fixed search modal

--- shop_site_fe/src/components/modals/Search.jsx
+++ shop_site_fe/src/components/modals/Search.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Searcher from '../../utils/images/search-icon-other.png'
 import X from '../../utils/images/X.png';
 import '../../styles/search.css';
 
 const Search = props => {
     return (
         <div className="searchBar">
+            <img className="x" src={Searcher} alt="magnifing glass" onClick={() => {}} />
             <form onSubmit={props.handleSubmit}>
                 <input
                     type="text"
@@ -14,7 +16,7 @@ const Search = props => {
                     onChange={props.handleChange}
                 />
             </form>
-            <img className="x" src={X} alt="X" onClick={() => props.showSearching(false)} />
+            <img className="x" src={X} alt="X" onClick={() => {props.showSearching(false)}} />
         </div>
     )
 }
