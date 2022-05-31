ecb313268200b8965e7374c8a5a77567b56c0151
Trey McGarity <trey.mcgarity99@gmail.com>
2022-05-30 21:22:10 -0500

    added categories to flow with listing menu, filling and styling tom

---
+++ shop_site_fe/src/components/modals/patron-modals/Categories.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../../styles/modals/patron-modals/listing.scss';
+
+const Categories = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Categories;
