a87dd176ffb9df20da51d9453d5835802cbbf457
Trey McGarity <trey.mcgarity99@gmail.com>
2023-01-30 22:43:50 -0600

    home styling updates

--- shop_site_fe/src/components/pages/patron-pages/Home.jsx
+++ shop_site_fe/src/components/pages/patron-pages/Home.jsx
@@ -5,6 +5,11 @@ import Product from '../segments/Product';
 import { connect, useDispatch } from "react-redux";
 import { getProducts } from '../../../redux/actions/productActions';
 import Group from '../../../utils/images/home/group_setting.jpg';
+import House from '../../../utils/images/home/house.png';
+import Plant from '../../../utils/images/home/plant.png';
+import Shirt from '../../../utils/images/home/shirt.png';
+import Pants from '../../../utils/images/home/joggers.png';
+import Picture from '../../../utils/images/home/picture.png';
 import Model1 from '../../../utils/images/home/models/mode_1.jpg';
 import '../../../styles/pages/patron-pages/home.scss';
 
@@ -19,11 +24,18 @@ const Home = (props) => {
         <>
         <Header />
         <div className='home'>
-            {/* <div className='promo'>
+            <div className='promo'>
                 <h1>Free Shipping on Orders over $30</h1>
                 <p>Must have an account to access incredible deal offer!</p>
-            </div> */}
+            </div>
             <img src={Group} alt='group setting'/>
+            <div className='icon-menu'>
+                <img className='icon' src={Plant} alt='plants icon' />
+                <img className='icon' src={Picture} alt='painting icon' />
+                <img className='icon' src={House} alt='house icon' />
+                <img className='icon' src={Shirt} alt='shirt icon' />
+                <img className='icon' src={Pants} alt='pants icon' />
+            </div>
             <div className='model'>
                 <img src={Model1} alt='model with peace sign'/>
             </div>
