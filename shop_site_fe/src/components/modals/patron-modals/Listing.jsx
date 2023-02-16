dd9bcc1efeb6863d2029e98ba7fe87f25053e81d
Trey McGarity <trey.mcgarity99@gmail.com>
2023-02-16 15:36:06 -0600

    updated listing menu

--- shop_site_fe/src/components/modals/patron-modals/Listing.jsx
+++ shop_site_fe/src/components/modals/patron-modals/Listing.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import '../../../styles/modals/patron-modals/listing.scss';
 
 const Listing = () => {
     return (
         <div className='listing'>
-            <h1 className='options'>Browse</h1>
-            <h1 className='options'>New</h1>
-            <h1 className='options'>Popular</h1>
-            <h1 className='options'>Wall Art</h1>
-            <h1 className='options'>Floating Decor</h1>
-            <h1 className='options'>Clothing</h1>
-            <h1 className='options'>Collections</h1>
+            <Link to='/home' className='options'>Home</Link>
+            <Link to='/browsing' className='options'>Browse</Link>
+            <Link to='/browsing' className='options'>New</Link>
+            <Link to='/browsing' className='options'>Popular</Link>
+            <Link to='/browsing' className='options'>Wall Art</Link>
+            <Link to='/browsing' className='options'>Collections</Link>
         </div>
     )
 }
